6bc291778600c146283446f8784f4dd96c79a4d9
Breno Estrella <brenogbestrella@gmail.com>
2021-03-08 21:30:06 -0300

    Arquivos Aula 11

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const listaUsuarios = [
+    'Breno',
+    'Ivens',
+    'Jailson',
+    'Nykolle',
+];
+const path = require('path');
+
+//Query Params
+//Ex:localhost:12345/usuarios?nome=Breno
+app.get('/usuarios', (req, res) => {
+    const { nome } = req.query;
+    let listaRetorno = listaUsuarios.filter(i => i.includes(nome || ''))
+    return res.json(listaRetorno)
+});
+
+
+//Route Params
+//Ex: ecommer.com.br/produtos/4/detalhes
+//app.get('/hello/:usuario', (req,res) => {
+    //const { usuario } = req.params;
+    //return res.send(`Hello world ${usuario}`);   //usar return como boa prática
+//});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/src/views/index.html'));
+});
+
+
+app.listen(12345, () => {
+    console.log('Servidor rodando na porta 12345')
+});
